a76b7ccaa35528a737c91901bd7a85e121a65022
LuisChen <leo5916267@gmail.com>
2018-05-27 13:25:27 +0800

    建立手腳架檔案位置

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,7 +2,7 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
-
+const _ = require('lodash');
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
@@ -32,8 +32,51 @@ module.exports = class extends Generator {
   writing() {
     this.fs.copy(
       this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.destinationPath('dummyfile.txt'),
+      this.initPackage(),
+      this.renderTplFile()
     );
+  }
+  initPackage() {
+    let pkg = this.fs.readJSON(this.templatePath('package.json'), {});
+    const { props } = this;
+
+    pkg = _.merge(pkg, {
+      name: props.name,
+      description: props.description
+    });
+
+    this.fs.writeJSON(this.destinationPath('package.json'), pkg);
+  }
+
+  renderTplFile() {
+    let target = [
+      ['_babelrc', '.babelrc'],
+      ['_editorconfig', '.editorconfig'],
+      ['_gitignore', '.gitignore'],
+      ['_postcssrc.js', '.postcssrc.js'],
+      'database/people.json',
+      'public/favicon.ico',
+      'public/index.html',
+      'public/manifest,json',
+      'src/page/home/components/home.js',
+      'src/page/home/containers/home.js',
+      'src/page/home/store/action.js',
+      'src/page/home/store/actionType.js',
+      'src/page/home/store/epic.js',
+      'src/page/home/store/reducer.js',
+      'src/routes/index.js',
+      'src/store/configureStore.js',
+      'src/store/epic.js',
+      'src/store/reducer.js',
+      'src/app.js',
+      'src/index.js',
+      'src/registerServiceWorker.js'
+    ];
+
+    _.forEach(target, file => {
+      this.fs.copyTpl(this.templatePath(file), this.destinationPath(file), this.props);
+    });
   }
 
   install() {
