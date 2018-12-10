6bda16c9904d5a68c481aeab0a3ff074ac5a954d
LuisChen <leo5916267@gmail.com>
2018-12-10 10:29:48 +0800

    fix

--- generators/app/templates/src/app.js
+++ generators/app/templates/src/app.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css';
 
 import PagesRouter from './routes';
 
 const App = () => {
-  return (
-    <div>
-      <header>
-        <Link to="/">Home</Link>
-        <Link to="/about-us">About</Link>
-      </header>
-      <main>
-        <PagesRouter />
-      </main>
-    </div>
-  );
+	return (
+		<div>
+			<header>
+				<Link to="/">Home</Link>
+				<Link to="/about-us">About</Link>
+			</header>
+			<main>
+				<PagesRouter />
+			</main>
+		</div>
+	);
 };
 
 export default App;
