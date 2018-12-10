b8cc5f0a732a10eeef96784058d3f64d22e09e4f
LuisChen <leo5916267@gmail.com>
2018-12-10 10:08:03 +0800

    增加 firebase pakage

--- generators/app/templates/src/store/configureStore.js
+++ generators/app/templates/src/store/configureStore.js
@@ -4,8 +4,50 @@ import { createEpicMiddleware } from 'redux-observable';
 import { createLogger } from 'redux-logger';
 import { createBrowserHistory } from 'history';
 
+/**
+ * firebase
+ */
+
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import { reduxFirestore } from 'redux-firestore'
+import firebase from 'firebase'
+import 'firebase/firestore' // <- needed if using firestore
+
 import rootReducer from './reducer';
 import rootEpic from './epic';
+
+/**
+ * react-redux-firebase
+ */
+
+// const firebaseConfig = {
+
+// }
+// firebase.initializeApp(firebaseConfig)
+// firebase.firestore().settings({ timestampsInSnapshots: true })
+
+// const reactReduxFirebaseConfig = {
+// 	userProfile: 'users', // firebase root where user profiles are stored
+// 	// enableLogging: true, // enable/disable Firebase's database logging
+// 	attachAuthIsReady: true, // attaches auth is ready promise to store,
+// 	fileMetadataFactory: uploadRes => {
+// 		// upload response from Firebase's storage upload
+// 		const {
+// 			metadata: { name, fullPath, downloadURLs }
+// 		} = uploadRes
+// 		// default factory includes name, fullPath, downloadURL
+// 		return {
+// 			name,
+// 			fullPath,
+// 			downloadURL: downloadURLs[0]
+// 		}
+// 	}
+// }
+
+// const createStoreWithFirebase = compose(
+// 	reactReduxFirebase(firebase, reactReduxFirebaseConfig), // firebase instance as first argument
+// 	reduxFirestore(firebase) // <- needed if using firestore
+// )(createStore)
 
 const epicMiddleware = createEpicMiddleware();
 
@@ -24,6 +66,8 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 const composedEnhancers = compose(
+  // reactReduxFirebase(firebase, reactReduxFirebaseConfig),
+	// reduxFirestore(firebase),
   applyMiddleware(...middleware),
   ...enhancers
 );
