d0fd34f7c0fbfcd79c2cd72f316792fbfdbe2a91
LuisChen <leo5916267@gmail.com>
2018-12-10 10:11:39 +0800

    新增 persist

--- generators/app/templates/src/store/configureStore.js
+++ generators/app/templates/src/store/configureStore.js
@@ -13,6 +13,15 @@ import { reduxFirestore } from 'redux-firestore'
 import firebase from 'firebase'
 import 'firebase/firestore' // <- needed if using firestore
 
+/**
+ * localStorge
+ */
+
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+
+
 import rootReducer from './reducer';
 import rootEpic from './epic';
 
@@ -49,13 +58,29 @@ import rootEpic from './epic';
 // 	reduxFirestore(firebase) // <- needed if using firestore
 // )(createStore)
 
+/**
+ * redux-persist
+ */
+
+// const persistConfig = {
+// 	key: 'root',
+// 	storage: storage,
+// 	stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
+// 	whitelist: ['cart']
+// }
+
+
 const epicMiddleware = createEpicMiddleware();
 
 export const history = createBrowserHistory();
 
+// const persistedReducer = persistReducer(persistConfig, rootReducer(history))
+
 const initialState = {};
 const enhancers = [];
 const middleware = [routerMiddleware(history), epicMiddleware];
+// firebase
+// const middleware = [routerMiddleware(history), thunk.withExtraArgument(getFirebase), epicMiddleware]
 
 if (process.env.NODE_ENV === 'development') {
   const devToolsExtension = window.devToolsExtension;
@@ -75,3 +100,13 @@ const composedEnhancers = compose(
 epicMiddleware.run(rootEpic);
 
 export const store = createStore(rootReducer(history), initialState, composedEnhancers);
+
+/**
+ * firebase / persitor
+ */
+// export const store = createStoreWithFirebase(
+// 	persistedReducer, // root reducer with router state
+// 	initialState,
+// 	composedEnhancers
+// )
+// export const persistor = persistStore(store)
